1d211f86f6eb3d304b6da8f9693921f78600f750
weariccha <admin@theiccha.com>
2026-09-07 01:11:19 +0400

    Delete public directory

--- public/js/config.js
+++
@@ -1,5 +0,0 @@
-// Production backend
-window.ICCHA_CHECKOUT_BACKEND = "https://iccha-backend.onrender.com";
-
-// Keep API_BASE also (because api.js uses it)
-const API_BASE = window.ICCHA_CHECKOUT_BACKEND;
